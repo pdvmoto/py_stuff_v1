2925532239ab65d2b6ee2cabdc9300971ad1180e
pdvbv macbook <piet.devisser@cheerful.com>
2021-06-05 15:54:03 +0200

    pslist

--- py_pslist.py
+++ py_pslist.py
@@ -92,12 +92,12 @@ print ( f_prfx() )
 
 
 # ps_temps = psutil.sensors_temperatures()
-ps_cpu_tim = psutil.cpu_times() 
+# ps_cpu_tim = psutil.cpu_times() 
 # print ( f_prfx(), "CPU times: ", ps_cpu_tim )
 
 
-ps_cpu_phys = psutil.cpu_count(logical=False)
-ps_cpu_log  = psutil.cpu_count(logical=True)
+# ps_cpu_phys = psutil.cpu_count(logical=False)
+# ps_cpu_log  = psutil.cpu_count(logical=True)
 # print ( f_prfx(), "CPU count, logical and phys", ps_cpu_log, ps_cpu_phys )
 
 # print ( f_prfx(), "cpu freq " , psutil.cpu_freq(percpu=True) )
@@ -109,27 +109,32 @@ ps_cpu_log  = psutil.cpu_count(logical=True)
 
 # hit_enter = input ( f_prfx() + "now measring cpu 3sec " + "...., hit enter.." )
 
-for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent'] ):
+for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent', 'ppid', 'num_threads' ] ):
   # print( f_prfx(), " proc info: ", proc.info)
   # f_inspect_obj( 'proc_info', proc.info )
   proc_cpu_perc = proc.info.get('cpu_percent') 
   proc_pid      = proc.info.get('pid') 
-  proc_name      = proc.info.get('name') 
+  proc_ppid     = proc.info.get('ppid') 
+  proc_name     = proc.info.get('name') 
   # print ( f_prfx(), " pid + cpu percent: ", proc_pid, proc_cpu_perc, proc_name )
   
 # initialized counters and properties ..
 
-tim.sleep ( 5) 
+tim.sleep ( 3) 
 
+# initiate
 perc_total = 0.0
+pslist=[] 
 
 # 2nd call.. to have meaningful values
-for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent'] ):
+for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent', 'ppid', 'num_threads' ] ):
   # print( f_prfx(), " proc info: ", proc.info)
   # f_inspect_obj( 'proc_info', proc.info )
   proc_cpu_perc = proc.info.get('cpu_percent') 
   proc_pid      = proc.info.get('pid') 
-  proc_name      = proc.info.get('name') 
+  proc_ppid     = proc.info.get('ppid') 
+  proc_name     = proc.info.get('name') 
+  proc_nthreads = proc.info.get('num_threads') 
 
   if not isinstance(proc_cpu_perc, float):
     f_perc = 0.0
@@ -137,15 +142,39 @@ for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent'] ):
     f_perc = float ( proc_cpu_perc ) 
     perc_total = perc_total + f_perc
   
-  if f_perc > 1.0 :
-    print ( f_prfx(), " pid + cpu percent: ", proc_pid, f_perc, proc_name )
+  if f_perc > 0.2 :
+    # print ( f_prfx(), " pid + cpu percent: ", repr(proc_ppid).rjust(5), repr(proc_pid).rjust(7), '{:7.1f}'.format ( f_perc ), " " + proc_name )
+    pslist.append ( proc.info ) 
   
-# 2nd loop, printed values above threshold
+# end 2nd loop, printed values above threshold
+
+# try sorting the collected list, highest cpu-perc at bottom of list
+pslist.sort( key=lambda x: (x['cpu_percent'], x['ppid'], x['pid']) ) 
+
+# print ( pslist )
+# f_inspect_obj( 'pslist after sorting: ', pslist )
+
+for  psinfo in pslist :
+  proc_cpu_perc = psinfo.get('cpu_percent') 
+  proc_pid      = psinfo.get('pid') 
+  proc_ppid     = psinfo.get('ppid') 
+  proc_name     = psinfo.get('name') 
+  proc_nthreads = psinfo.get('num_threads')
+  f_perc = float ( proc_cpu_perc ) 
+  print ( f_prfx(), " pid + cpu percent: ", repr(proc_ppid).rjust(5), repr(proc_pid).rjust(7), '{:7.1f}'.format ( f_perc ), " ", proc_name , "(", proc_nthreads, ")" )
+
+# end for, printed ps-data in order of sort
+
+# print ( f_prfx(), " -------- End of Loop -------- " )
+# print ( f_prfx() )
+
+# show the last one, inspect
+# f_inspect_obj( 'proc_info', proc.info )
 
 print ( f_prfx() )
-print ( f_prfx(), " Total cpu-perc", perc_total )
+print ( f_prfx(), "                 Total cpu-perc", round ( perc_total, 2) )
 print ( f_prfx() )
-print ( f_prfx(), " -------- End -------- " )
+print ( f_prfx(), " -------- End of program -------- " )
 
 
 
