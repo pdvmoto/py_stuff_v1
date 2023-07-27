f70f4b463311a9eb6c18330f0d450bed74df8b6c
pdvbv macbook <piet.devisser@cheerful.com>
2023-07-27 16:45:10 +0200

    ok

--- py_pslist.py
+++ py_pslist.py
@@ -72,7 +72,7 @@ def f_inspect_obj( s_objname, o_obj ):
   print ( f_prfx(), "o_obj length: ",  len (o_obj ) )
   print ( f_prfx(), "o_obj dir   : ",  dir (o_obj ) )
   print ( " " )
-  hit_enter = input ( f_prfx() + "meta data from " + s_objname + "...., hit enter.." )
+  # hit_enter = input ( f_prfx() + "meta data from " + s_objname + "...., hit enter.." )
 
   print ( "--- repr --->" )
   print ( f_prfx(), "o_obj repr  : ",  repr ( o_obj ) )
@@ -102,7 +102,45 @@ print ( f_prfx() )
 
 # print ( f_prfx(), "cpu freq " , psutil.cpu_freq(percpu=True) )
 
+vmem = psutil.virtual_memory()
+
+# f_inspect_obj( 'vmem', vmem )
+
 # print ( f_prfx(), "virtual mem ", psutil.virtual_memory() )
+# print ( f_prfx(), "virtual mem ", vmem )
+
+vtotal = vmem.total     / ( 1024* 1024 )
+vactiv = vmem.active    / ( 1024* 1024 )
+vinact = vmem.inactive  / ( 1024* 1024 )
+vavail = vmem.available / ( 1024* 1024 )
+vused  = vmem.used      / ( 1024* 1024 )
+vwired = vmem.wired     / ( 1024* 1024 )
+vfree  = vmem.free      / ( 1024* 1024 )
+
+# print ( f_prfx(), 'vmem total: {:9.1f} M'.format ( vmem.total     / (1024*1024) ) ) 
+# print ( f_prfx(), 'vmem activ: {:9.1f} M'.format ( vmem.active    / (1024*1024) ) ) 
+# print ( f_prfx(), 'vmem inact: {:9.1f} M'.format ( vmem.inactive  / (1024*1024) ) ) 
+# print ( f_prfx(), 'vmem avail: {:9.1f} M'.format ( vmem.available / (1024*1024) ) ) 
+# print ( f_prfx(), 'vmem used : {:9.1f} M'.format ( vmem.used      / (1024*1024) ) ) 
+# print ( f_prfx(), 'vmem wired: {:9.1f} M'.format ( vmem.wired     / (1024*1024) ) ) 
+# print ( f_prfx(), 'vmem free : {:9.1f} M'.format ( vmem.free      / (1024*1024) ) ) 
+
+# print ( f_prfx(), 'vmem total: %10.2f', vmem.total) 
+# print ( f_prfx(), 'vmem  free: %10.2f', vmem.free) 
+
+smem = psutil.swap_memory()
+
+# f_inspect_obj( 'smem', smem )
+
+stotal= smem.total      / ( 1024*1024 )
+sused = smem.used       / ( 1024*1024 )
+sfree = smem.free       / ( 1024*1024 )
+
+# print ( f_prfx(), 'smem total: {:9.1f} M'.format ( stotal    ) ) 
+# print ( f_prfx(), 'smem used : {:9.1f} M'.format ( sused     ) ) 
+# print ( f_prfx(), 'smem free : {:9.1f} M'.format ( sfree     ) ) 
+
+# print ( f_prfx(), "   swap mem ", psutil.swap_memory() )
 
 # print ( f_prfx(), "temp: ", psutil.sensors_battery() )
 
@@ -120,7 +158,7 @@ for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent', 'ppi
   
 # initialized counters and properties ..
 
-tim.sleep ( 3) 
+tim.sleep ( 4) 
 
 # initiate
 perc_total = 0.0
@@ -142,7 +180,7 @@ for proc in psutil.process_iter( ['pid', 'name', 'username', 'cpu_percent', 'ppi
     f_perc = float ( proc_cpu_perc ) 
     perc_total = perc_total + f_perc
   
-  if f_perc > 0.2 :
+  if f_perc > 0.9 :
     # print ( f_prfx(), " pid + cpu percent: ", repr(proc_ppid).rjust(5), repr(proc_pid).rjust(7), '{:7.1f}'.format ( f_perc ), " " + proc_name )
     pslist.append ( proc.info ) 
   
@@ -172,7 +210,7 @@ for  psinfo in pslist :
 # f_inspect_obj( 'proc_info', proc.info )
 
 print ( f_prfx() )
-print ( f_prfx(), "                        Total cpu-perc", round ( perc_total, 2) )
+print ( f_prfx(), "                        Total cpu-perc", round ( perc_total, 2) , ' Free mem: ', round (vfree, 2), 'M'  )
 print ( f_prfx() )
 print ( f_prfx(), " -------- End of program -------- " )
 
